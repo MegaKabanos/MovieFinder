0a7dc986ed30f5b7f90c20376d6f21ef9754e295
MegaKabanos <wexler.8498@gmail.com>
2026-01-12 22:47:27 +0100

    minor fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react(), 
+    tailwindcss(), 
+    svgr()
+  ],
+  base: '/MovieFinder/',
 })
